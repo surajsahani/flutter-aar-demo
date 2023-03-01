03777c5a1bcd365a50f18f5c14c73ffb31b05a9d
suraj <suraj@unifynd>
2023-03-01 17:56:26 +0530

    aar sample

---
+++ android/app/src/main/kotlin/com/example/librarydemoappflutter/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.librarydemoappflutter
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+//        Logger.isEnabled = true
+//        Logger.d("MainActivity", "hello")
+    }
+}
